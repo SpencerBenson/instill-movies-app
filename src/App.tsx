b7b30aa4a7fbfea1122fcb2f648a7040884236f2
Benson Kariuki <bennspencer@gmail.com>
2023-09-07 12:32:35 +0300

    remove default vite content and add Movie search component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import MovieSearch from './MovieSearch';
+import 'bootstrap/dist/css/bootstrap.min.css'; // import bootstrap styling
+function App() {
+  return (
+    <div className="container">
+      <MovieSearch />
+    </div>
+  );
+}
+
+export default App;
